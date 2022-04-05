9054b6dcbaa03dd955c262111e302f9433596bbd
raauf <vrairaaufabidi@gmail.com>
2022-04-05 07:58:27 +0200

    une modification au niveau du fichier Connect.php

--- callWS/Connect.php
+++ callWS/Connect.php
@@ -2,16 +2,31 @@
 namespace callWs;
 use \SoapClient;
 class Connect{
+	private static $parametres=[];
 	private static $instance;
 	public static function get_connexion($configFile){
 		if(is_null(self::$instance)){
-			$parametres=$configFile;
-			$url=$parametres['url']?$parametres['url']:null;
-			self::$instance=new SoapClient($url, $parametres['options']);
-			
+			self::$parametres=require($configFile);
+			$url=self::$parametres['url']?self::$parametres['url']:null;
+			self::$instance = new SoapClient($url, self::$parametres['options']);
 		}
 		//die(var_dump(self::$instance->__getFunctions()));
 		return self::$instance;
 	}
+	/*
+	public function __construct($configFile){
+		$this->parametres=require($configFile);
+		$url=$this->parametres['url']?$this->parametres['url']:null;
+		self::$instance new SoapClient($url, $this->parametres['options']);
+		//die(var_dump($soap->__getFunctions()));
+	}
+	public function get($key)
+	{
+		if(in_array($key, $this->parametres)){
+			return $this->parametres[$key];
+		}
+		return "cette parametres introuvable";
+	}
+	*/
 }
 ?>
