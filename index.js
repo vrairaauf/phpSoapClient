176bb3731d3c812fdc3ec8ec7afd5117181a2405
raauf <vrairaaufabidi@gmail.com>
2022-04-05 08:02:51 +0200

    le script dans le fichier index.js est basée maintenant sur la fonction exec de la classe process_children apres le test unitaire ce classe est efficace dans cet cas car elle renvoie le resultat du script externe dans un objet de type string

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 let server=require('http');
-const {spawn} = require('child_process');
-function externScript(){
-		return spawn('php', ['route.php', 'helloWorld', '56']);
-}
-function appelle(){
-	var dataToSend;
-	const php= externScript();
-	php.stdout.on('data', function (data) {
-  	dataToSend = data.toJSON();
-  	console.log(dataToSend);
- 	});
-}
-appelle();
-/*
-	var dataToSend;
-	const python= externScript();
-	python.stdout.on('data', function (data) {
-  	dataToSend = data.toString();
-  	console.log(dataToSend);
- 	});
-	*/
+const {exec} = require('child_process');
+exec('php route.php -m LatLonListZipCode --args zipcodelist/city/country', (error, stdout, stderr)=>{
+	if(error){
+		console.log('error:'+error.message);
+		return;
+	}
+	if(stderr){
+		console.log('error: '+stderr);
+		return;
+	}
+	console.log('stdout :\n'+stdout);
+
+});
