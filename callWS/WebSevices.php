945b2fdd4bdedddacee16cf5ce6d93ca86311efb
raauf <vrairaaufabidi@gmail.com>
2022-04-05 07:59:23 +0200

    le fichier WebServices.php etre plus dynamique

--- callWS/WebSevices.php
+++ callWS/WebSevices.php
@@ -1,11 +1,13 @@
 <?php 
 namespace callWs;
+use \SoapClient;
 class WebServices{
 	private $connexion;
-	public function __construct($connexion){
+	public function __construct(SoapClient $connexion){
 		$this->connexion=$connexion;
 	}
-	public function apelleMethode($methode, $parametres){
+	public function apelleMethode($methode, $parametres=[]){
+		die(var_dump($parametres));
 		try {
     		$responce_param = $this->connexion->$methode($parametres);
     		return $responce_param;
