6f2b3cefbda7879726e5bea6fa38c3dc9975cf09
raauf <vrairaaufabidi@gmail.com>
2022-04-05 08:03:38 +0200

    un changement au niveau du fichier route.php afin de ces modifications route.php devient plus dynamique est reponde au besoin du concept de script [getopt()]

--- callWS/Connect.php
+++ callWS/Connect.php
@@ -13,20 +13,6 @@ class Connect{
 		//die(var_dump(self::$instance->__getFunctions()));
 		return self::$instance;
 	}
-	/*
-	public function __construct($configFile){
-		$this->parametres=require($configFile);
-		$url=$this->parametres['url']?$this->parametres['url']:null;
-		self::$instance new SoapClient($url, $this->parametres['options']);
-		//die(var_dump($soap->__getFunctions()));
-	}
-	public function get($key)
-	{
-		if(in_array($key, $this->parametres)){
-			return $this->parametres[$key];
-		}
-		return "cette parametres introuvable";
-	}
-	*/
+	
 }
 ?>
--- route.php
+++ route.php
@@ -1,10 +1,12 @@
 <?php
 require "controller/controller.php";
+$args=getopt("m:", ["args:"]);
 if($argc>1){
-	if(isItServices($argv[1])){
-		callRemoteWebServices($argv[1], $argv[2]);
+	if(isItServices($args['m'])){
+		var_dump($args['args']);
+		callRemoteWebServices($args['m'], explode("/", $args["args"]));
+	}else{
+		die("veiller entrer des parametres");
 	}
-}else{
-	die("veiller entrer des parametres");
 }
 ?>
